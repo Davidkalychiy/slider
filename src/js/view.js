3a76e3854fc7e471ed12afdad0c2af3b8d0c86e7
David <ash191919191919@gmail.com>
2020-06-09 20:51:35 +0300

    добавил возможность изменять значение слайдера через инпут

--- src/js/view.js
+++ src/js/view.js
@@ -2,8 +2,9 @@
 
 
 
-let wrapper = document.querySelector('.wrapper');
-
+let wrapper = document.querySelector('.wrapper'),
+    changeVerScale = document.getElementById('changeVerScale'),
+    changeHorScale = document.getElementById('changeHorScale');
 
 
 
@@ -47,15 +48,41 @@ let wrapper = document.querySelector('.wrapper');
 //view
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class scale {
     constructor(name_slider) {
         this.name_slider = name_slider;
         this.element = document.createElement('div');
     }
     render() {
-        this.element.innerHTML =`
-        <div class='${this.name_slider}'>
-        </div>`;
+        // this.element.style.width = '300px'
+        // this.element.style.height = '300px'
+        this.element.classList.add(this.name_slider);
         wrapper.append(this.element);
     }
     
@@ -86,9 +113,25 @@ slider_horizontal_1.render();
 // slider_horizontal_2.render();
 
 
+class underLine {
+    constructor(parent) {
+        this.parent = parent;
+        this.element = document.createElement('div');
+    }
+    render(underLine_class) {
+        this.element.classList.add(underLine_class);
+        this.parent.append(this.element);
+    }
+}
 
 
 
+let underLine_1 = new underLine(slider_horizontal_1.element);
+underLine_1.render('slider_horizontal__underLine');
+
+
+let underLine_2 = new underLine(slider_vertical_1.element);
+underLine_2.render('slider_vertical__underLine');
 
 
 
@@ -119,8 +162,8 @@ class round {
 
 
 
-let round_1 = new round(slider_vertical_1.element.querySelector('.' + slider_vertical_1.name_slider));
-let round_2 = new round(slider_horizontal_1.element.querySelector('.' + slider_horizontal_1.name_slider));
+let round_1 = new round(slider_vertical_1.element);
+let round_2 = new round(slider_horizontal_1.element);
 round_1.render('slider_vertical__round')
 round_2.render('slider_horizontal__round')
 
@@ -147,9 +190,6 @@ round_2.render('slider_horizontal__round')
 
 
 
-
-
-
 class value {
     constructor(parent) {
         this.parent = parent;
@@ -158,17 +198,19 @@ class value {
     render(value_class) {
         this.element.classList.add(value_class);
         this.parent.append(this.element);
+        
     }
 }
+
 
 
 // new value('.slider_horizontal', '.value').render();
 // let value_horizontal = new value('.slider_horizontal', '.slider_horizontal__value').value;
 
-let value_1 = new value(slider_vertical_1.element.querySelector('.' + slider_vertical_1.name_slider));
-let value_2 = new value(slider_horizontal_1.element.querySelector('.' + slider_horizontal_1.name_slider));
-value_1.render('slider_vertical__value');
-value_2.render('slider_horizontal__value');
+let value_1 = new value(slider_vertical_1.element);
+let value_2 = new value(slider_horizontal_1.element);
+// value_1.render('slider_vertical__value');
+// value_2.render('slider_horizontal__value');
 // let value_vertical_1 = new value('.slider_vertical', '.slider_vertical__value').value;
 // let value_vertical_2 = new value('.slider_vertical', '.slider_vertical__value').value;
 
@@ -179,10 +221,11 @@ value_2.render('slider_horizontal__value');
 
 
 class move {
-    constructor(round,scale,value,type_slider) {
+    constructor(round,scale,value,type_slider, underLine) {
         this.round = round;
         this.value = value;
         this.scale = scale;
+        this.underLine = underLine;
         this.type_slider = type_slider;
     }
     render(max,min,value_num,step) {
@@ -200,44 +243,113 @@ class move {
         //     value_num: 10,
         //     step: 10
         // };
+ 
         let scale = this.scale,
             round = this.round,
             value = this.value,
             slider = this.slider,
+            underLine = this.underLine,
             type_slider = this.type_slider;
 
         
+
+            const step_real = (scale.offsetWidth-round.offsetWidth)/((slider.max-slider.min)/slider.step); 
+            let date = [];
+            for (let k = scale.getBoundingClientRect().left, i = slider.min; i <= slider.max; i += slider.step, k+=step_real) {
+                date.push([k,i]);
+            }
+
+            
+
+
+
         if (type_slider == 'slider_vertical') {
 
+
+            changeVerScale.addEventListener('change', changeScaleValue);
+            
+            function changeScaleValue() {
+                date.forEach(i => {
+                    if (i[1] == +changeVerScale.value) {
+                        round.style.top = i[0] - scale.getBoundingClientRect().top + 'px';
+                    }
+                })
+            }
 
 
             value.textContent = slider.value_num;
             round.addEventListener('mousedown', mouseDown);
 
 
+            const step_real = (scale.offsetHeight-round.offsetHeight)/((slider.max-slider.min)/slider.step); 
+            let date = [];
+            for (let k = scale.getBoundingClientRect().top, i = slider.min; i <= slider.max; i += slider.step, k+=step_real) {
+                date.push([k,i]);
+            }
+
+            date.forEach(element => {  
+                let div = document.createElement('div');
+                div.style.display = 'flex'
+                div.style.justifyContent = 'center'
+                div.style.alignItems = 'center'
+                div.style.position = 'absolute'
+                div.style.top = element[0] - scale.getBoundingClientRect().top + 'px'
+                div.append(element[1]);
+                div.style.width = '15px'
+                div.style.height = '15px'
+                underLine.append(div)
+                div.style.userSelect = 'none'
+            })
+
+            scale.addEventListener('click', onClickScale);
 
 
             function mouseMove(event) {
-                console.log(slider.max)
-                const step_real = (scale.offsetHeight-round.offsetHeight)/((slider.max-slider.min)/slider.step); 
-                let date = [];
-                for (let k = scale.getBoundingClientRect().top, i = slider.min; i <= slider.max; i += slider.step, k+=step_real) {
-                    date.push([k,i]);
+       
+                if(event.pageY <= scale.getBoundingClientRect().top) {
+                    round.style.top= 0 + 'px';
+                    // value.style.top = 0 + 'px';
+                    slider.value_num = slider.min;
+                    value.textContent = slider.value_num;
+                    changeVerScale.value = slider.value_num;
+                }else if (event.pageY >= scale.getBoundingClientRect().top + scale.offsetHeight) {
+                    round.style.top = date[date.length-1][0] - scale.getBoundingClientRect().top + 'px';
+                    // value.style.top = date[date.length-1][0] - scale.getBoundingClientRect().top + 'px';
+                    slider.value_num = slider.max;
+                    value.textContent = slider.value_num;
+                    changeVerScale.value = slider.value_num;
                 }
                 date.forEach(element => {
-                    if (event.clientY > element[0] - step_real && event.clientY < element[0] + step_real) {
-                    
-                        if(event.clientY <= scale.getBoundingClientRect().top) {
-                            round.style.top = 0 + 'px';
-                            value.style.top = 0 + 'px';
-                            slider.value_num = slider.min;
-                            value.textContent = slider.value_num;
-                        }else {
-                            round.style.top = element[0] - scale.getBoundingClientRect().top + 'px' ;
-                            value.style.top = element[0] - scale.getBoundingClientRect().top + 'px' ;
-                            slider.value_num = element[0];
-                            value.textContent = element[1] ;
-                        }
+                    if (event.pageY > element[0] - step_real && event.pageY < element[0] + step_real) {
+                        round.style.top = element[0] - scale.getBoundingClientRect().top + 'px' ;
+                        // value.style.top = element[0] - scale.getBoundingClientRect().top + 'px' ;
+                        slider.value_num = element[1];
+                        value.textContent = element[1];
+                        changeVerScale.value = slider.value_num;
+                    }
+                });
+            }
+
+
+            function onClickScale(event) {
+                if(event.pageY <= scale.getBoundingClientRect().top) {
+                    round.style.top = 0 + 'px';
+                    value.style.top = 0 + 'px';
+                    slider.value_num = slider.min;
+                    value.textContent = slider.value_num;
+                }else if (event.pageY >= scale.getBoundingClientRect().top + scale.offsetHeight) {
+                    round.style.top = date[date.length-1][0] - scale.getBoundingClientRect().top + 'px';
+                    value.style.top = date[date.length-1][0] - scale.getBoundingClientRect().top + 'px';
+                    slider.value_num = slider.max;
+                    value.textContent = slider.value_num;
+                }
+                date.forEach(element => {
+                    if (event.pageY > element[0] - step_real/2 && event.pageY < element[0] + step_real/2) {
+                        round.style.top = element[0] - scale.getBoundingClientRect().top + 'px' ;
+                        value.style.top = element[0] - scale.getBoundingClientRect().top + 'px' ;
+                        slider.value_num = element[1];
+                        value.textContent = element[1] ;
+
                     }
                 });
             }
@@ -258,34 +370,94 @@ class move {
             function mouseUp() {
                 document.removeEventListener('mousemove', mouseMove);
             }
+
+
+
+
         }else {
 
+
+            changeHorScale.addEventListener('change', changeScaleValue);
+            
+            function changeScaleValue() {
+                date.forEach(i => {
+                    if (i[1] == +changeHorScale.value) {
+                        round.style.left = i[0] - scale.getBoundingClientRect().left + 'px';
+                    }
+                })
+            }
             value.textContent = slider.value_num;
             round.addEventListener('mousedown', mouseDown);
 
 
+            scale.addEventListener('click', onClickScale);
 
+            const step_real = (scale.offsetWidth-round.offsetWidth)/((slider.max-slider.min)/slider.step); 
+            let date = [];
+            for (let k = scale.getBoundingClientRect().left, i = slider.min; i <= slider.max; i += slider.step, k+=step_real) {
+                date.push([k,i]);
+            }
+            date.forEach(element => {  
+                let div = document.createElement('div');
+                div.style.position = 'absolute'
+                div.style.left = element[0] - scale.getBoundingClientRect().left + 'px'
+                div.append(element[1]);
+                div.style.width = '15px'
+                div.style.textAlign = 'center'
+                underLine.append(div)
+                div.style.userSelect = 'none'
+            })
 
             function mouseMove(event) {
-                const step_real = (scale.offsetWidth-round.offsetWidth)/((slider.max-slider.min)/slider.step); 
-                let date = [];
-                for (let k = scale.getBoundingClientRect().left, i = slider.min; i <= slider.max; i += slider.step, k+=step_real) {
-                    date.push([k,i]);
+
+                if(event.pageX <= scale.getBoundingClientRect().left) {
+                    round.style.left = 0 + 'px';
+                    // value.style.left = 0 + 'px';
+                    slider.value_num = slider.min;
+                    value.textContent = slider.value_num;
+                    changeHorScale.value = slider.value_num;
+                }else if (event.pageX >= scale.getBoundingClientRect().left + scale.offsetWidth) {
+                    round.style.left = date[date.length-1][0] - scale.getBoundingClientRect().left + 'px';
+                    // value.style.left = date[date.length-1][0] - scale.getBoundingClientRect().left + 'px';
+                    slider.value_num = slider.max;
+                    value.textContent = slider.value_num;
+                    changeHorScale.value = slider.value_num;
                 }
                 date.forEach(element => {
-                    if (event.clientX > element[0] - step_real && event.clientX < element[0] + step_real) {
-                    
-                        if(event.clientX <= scale.getBoundingClientRect().left) {
-                            round.style.left = 0 + 'px';
-                            value.style.left = 0 + 'px';
-                            slider.value_num = slider.min;
-                            value.textContent = slider.value_num;
-                        }else {
-                            round.style.left = element[0] - scale.getBoundingClientRect().left + 'px' ;
-                            value.style.left = element[0] - scale.getBoundingClientRect().left + 'px' ;
-                            slider.value_num = element[0];
-                            value.textContent = element[1] ;
-                        }
+                    if (event.pageX > element[0] - step_real && event.pageX < element[0] + step_real) {
+                        round.style.left = element[0] - scale.getBoundingClientRect().left + 'px' ;
+                        // value.style.left = element[0] - scale.getBoundingClientRect().left + 'px' ;
+                        slider.value_num = element[1];
+                        value.textContent = element[1] ;
+                        
+                        changeHorScale.value = slider.value_num;
+
+                    }
+                });
+            }
+            
+
+
+            
+            function onClickScale(event) {
+                if(event.pageX <= scale.getBoundingClientRect().left) {
+                    round.style.left = 0 + 'px';
+                    // value.style.left = 0 + 'px';
+                    slider.value_num = slider.min;
+                    value.textContent = slider.value_num;
+                }else if (event.pageX >= scale.getBoundingClientRect().left + scale.offsetWidth) {
+                    round.style.left = date[date.length-1][0] - scale.getBoundingClientRect().left + 'px';
+                    // value.style.left = date[date.length-1][0] - scale.getBoundingClientRect().left + 'px';
+                    slider.value_num = slider.max;
+                    value.textContent = slider.value_num;
+                }
+                date.forEach(element => {
+                    if (event.pageX > element[0] - step_real/2 && event.pageX < element[0] + step_real/2) {
+                        round.style.left = element[0] - scale.getBoundingClientRect().left + 'px' ;
+                        // value.style.left = element[0] - scale.getBoundingClientRect().left + 'px' ;
+                        slider.value_num = element[0];
+                        value.textContent = element[1] ;
+
                     }
                 });
             }
@@ -339,8 +511,8 @@ class move {
 
 
 
-new move(round_1.element, slider_vertical_1.element.querySelector('.' + slider_vertical_1.name_slider), value_1.element, 'slider_vertical').render(1000,100,100,100)
-new move(round_2.element, slider_horizontal_1.element.querySelector('.' + slider_horizontal_1.name_slider), value_2.element, 'slider_horizontal').render(1000,10,10,10)
+new move(round_1.element, slider_vertical_1.element,'', 'slider_vertical', underLine_2.element).render(100,0,0,20)
+new move(round_2.element, slider_horizontal_1.element,'', 'slider_horizontal', underLine_1.element).render(100,0,0,10)
 
 
 
@@ -370,6 +542,5 @@ new move(round_2.element, slider_horizontal_1.element.querySelector('.' + slider
 
 
 //controller
-
 
 
